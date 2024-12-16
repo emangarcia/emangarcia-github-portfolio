8a70cf87b045d9e233b4b409d47d6c1d937fd5b1
Emmanuel Garcia <mr.eman.garcia@gmail.com>
2024-12-16 08:50:10 -0800

    update config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,6 +29,8 @@ export default defineNuxtConfig({
       link: [],
       script: [],
     },
+    baseURL: "/emangarcia-github-portfolio/", // baseURL: '/<repository>/'
+    buildAssetsDir: "assets", // don't use "_" at the begining of the folder name to avoids nojkill conflict
   },
   postcss: {
     plugins: {
